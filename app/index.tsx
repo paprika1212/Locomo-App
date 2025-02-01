db298dedc45a6be43141270c25f21ccb1850b8d3
ananditasharmaa <221210018@nitdelhi.ac.in>
2025-02-02 01:26:27 +0530

    changed background and text colour.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet} from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen. Hello guys!!!</Text>
+      <Text style={styles.text}>Hello guys!!!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e"
+  },
+  text: {
+    color: "#ffffff"
+  }
+});
+
