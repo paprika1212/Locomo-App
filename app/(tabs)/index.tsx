a19ae135399dbded001e4d217591a64b0d34492b
paprika1212 <aadrika.python@gmail.com>
2025-02-08 23:19:19 +0530

    tailwindconfig

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,42 +1,9 @@
-import { Text, View, StyleSheet} from "react-native";
-import { Link } from "expo-router";
+import { View, Text } from "react-native";
 
-export default function HomeScreen() {
+export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.text}>Hello guys!!! This is map Screen</Text>
-      <Link href= "/home" style={styles.button}> Go to Home now</Link>
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="text-white text-2xl">Welcome to Locomo App!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#25292e',
-  },
-  logo: {
-    width: 150,
-    height: 150, 
-    marginBottom: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-  text: {
-    color: "#ffffff"
-  },
-  button: {
-    color: "#ffffff",
-    padding: 10,
-    backgroundColor: "#0077cc",
-    borderRadius: 5,
-    marginTop: 10
-  }
-});
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{ts,tsx}"],  // Ensures Tailwind works in your app folder
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
